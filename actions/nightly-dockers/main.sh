dfcb6e525d06f0787a64c04752ed53387b680680
Ryan Cook <rcook@redhat.com>
2025-02-28 11:31:08 -0500

    nightly docker build
    Signed-off-by: Ryan Cook <rcook@redhat.com>

---
+++ actions/nightly-dockers/main.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Version will be nightly + timestamp
+VERSION=nightly-$(date +%Y%m%d)
+TEMPLATES=${TEMPLATES:-}
+
+set -euo pipefail
+
+if release_exists "test.pypi"; then
+  echo "Version $VERSION found in test.pypi"
+  PYPI_SOURCE="testpypi"
+elif release_exists "pypi"; then
+  echo "Version $VERSION found in pypi"
+  PYPI_SOURCE="pypi"
+else
+  echo "Version $VERSION not found in either test.pypi or pypi" >&2
+  exit 1
+fi
+
+set -x
+uv venv -p python3.10
+source .venv/bin/activate
+pip install -U .
+which llama
+llama stack list-apis
+
+build_and_push_docker() {
+  template=$1
+
+  USE_COPY_NOT_MOUNT=true LLAMA_STACK_DIR=. llama stack build --template $template --image-type container
+  docker images
+
+  echo "Pushing docker image"
+  docker tag distribution-$template:${VERSION} llamastack/distribution-$template:${VERSION}
+  docker tag distribution-$template:${VERSION} llamastack/distribution-$template:latest
+  docker push llamastack/distribution-$template:${VERSION}
+  docker push llamastack/distribution-$template:latest
+}
+
+
+if [ -z "$TEMPLATES" ]; then
+  TEMPLATES=(ollama together fireworks bedrock remote-vllm tgi meta-reference-gpu)
+else
+  TEMPLATES=(${TEMPLATES//,/ })
+fi
+
+for template in "${TEMPLATES[@]}"; do
+  build_and_push_docker $template
+done
+
+echo "Done"
